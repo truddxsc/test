319e2ce7b2a887ca3c021564ba1229ea09132a23
monostylar <xtur16+monostylar@gmail.com>
2024-10-16 16:55:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-import undetected_chromedriver as uc  # Import undetected_chromedriver
+import undetected_chromedriver as uc  # Correct import
 
 # Function to generate a random site name
 def generate_random_site_name():
