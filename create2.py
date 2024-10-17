194c8c1fc4ae2a71fc5e9b75c8764541d4ac2468
monostylar <xtur16+monostylar@gmail.com>
2024-10-17 07:54:26 +0700

    Update create2.py

--- create2.py
+++ create2.py
@@ -132,8 +132,8 @@ def fill_in_additional_details(driver, email):
 if __name__ == '__main__':
     generated_emails = []
     
-    # Buat 10 email acak
-    for _ in range(10):
+    # Buat 15 email acak
+    for _ in range(15):
         random_name = generate_random_name()
         new_email = f"mr.platra10+{random_name}@butyusa.com"
         generated_emails.append(new_email)
